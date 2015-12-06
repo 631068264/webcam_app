e758d4e17bd6e82f6b22640f67d08e74ac22d3aa
631068264 <631068264@qq.com>
2015-12-06 10:46:12 +0800

    init project

---
+++ app/src/main/java/com/example/wuyuxi/webcam/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.wuyuxi.webcam;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    private ActionBar actionBar;
+
+    @SuppressLint("JavascriptInterface")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        actionBar = getActionBar();
+        actionBar.hide();
+
+
+        webView = (WebView) findViewById(R.id.webview);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setPluginState(WebSettings.PluginState.ON);
+//        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+//        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.addJavascriptInterface(Jsinterface(), "ANDROIDAPI");
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+
+        webView.loadUrl("http://192.168.1.163/webcam/app");
+    }
+
+    private Object Jsinterface() {
+        Object object = new Object() {
+            @JavascriptInterface
+            public void playVideo(String url) {
+                Intent intent = new Intent(MainActivity.this, VideoActivity.class);
+                intent.putExtra("url", url);
+                startActivity(intent);
+            }
+        };
+        return object;
+    }
+
+
+    class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        public void onPageFinished(WebView view, String url) {
+        }
+    }
+
+
+}
+
+
---
+++ app/src/main/java/com/example/wuyuxi/webcam/VideoActivity.java
@@ -0,0 +1,99 @@
+package com.example.wuyuxi.webcam;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+/**
+ * Created by wuyuxi on 2015/11/27.
+ */
+public class VideoActivity extends Activity {
+    private SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    MediaPlayer mediaPlayer;
+    private Button back;
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+
+//        surfaceView = (SurfaceView) findViewById(R.id.video);
+
+        videoView = (VideoView) findViewById(R.id.video);
+
+        back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Close", "video_close");
+                videoView.stopPlayback();
+                finish();
+            }
+        });
+
+        String url = getIntent().getStringExtra("url");
+        Log.d("device_url",url);
+//        url = "rtsp://218.204.223.237:554/live/1/66251FC11353191F/e7ooqwcfbqjoo80j.sdp";
+        videoView.setVideoURI(Uri.parse(url));
+        videoView.setMediaController(new MediaController(this));
+        videoView.requestFocus();
+        videoView.start();
+
+//        surfaceHolder = surfaceView.getHolder();
+//        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+//        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+//            @Override
+//            public void surfaceCreated(SurfaceHolder holder) {
+//                mediaPlayer = new MediaPlayer();
+//                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//                mediaPlayer.setDisplay(surfaceHolder);
+//
+//                try {
+//                    mediaPlayer.reset();
+//                    mediaPlayer.setDataSource(url);
+//                    mediaPlayer.prepare();
+//                    mediaPlayer.start();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            @Override
+//            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+//
+//            }
+//
+//            @Override
+//            public void surfaceDestroyed(SurfaceHolder holder) {
+//
+//            }
+//        });
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+//        if (this.mediaPlayer != null) {
+//            this.mediaPlayer.release();
+//            this.mediaPlayer = null;
+//        }
+
+    }
+
+
+}
